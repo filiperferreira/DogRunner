49549ed30839554519aae3d0be31e73f749e6837
Filipe Ferreira <frferreira9@gmail.com>
2019-03-30 14:34:28 -0700

    Edited dog

--- DogRunner/Scenes/Level.gd
+++ DogRunner/Scenes/Level.gd
@@ -9,7 +9,7 @@ func _process(delta):
 	move_ground(delta)
 
 func create_ground():
-	for i in range(0,1430,64):
+	for i in range(-256,1430,64):
 		var ground = ground_scene.instance()
 		ground.position = Vector2(i, 736)
 		$GroundFolder.add_child(ground)
@@ -18,7 +18,7 @@ func move_ground(delta):
 	var ground_array = $GroundFolder.get_children()
 	for ground in ground_array:
 		ground.position.x -= 256 * delta
-		if ground.position.x <= -32:
+		if ground.position.x <= -256:
 			ground.queue_free()
 			var new_ground = ground_scene.instance()
 			new_ground.position = Vector2(ground_array[ground_array.size() - 1].position.x + 64 - 256 * delta, 736)
