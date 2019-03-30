52d53688b42be0d6416ae0ebba34ac92dd498090
Filipe Ferreira <frferreira9@gmail.com>
2019-03-30 13:11:38 -0700

    Added dog to level
    Dog can jump

---
+++ DogRunner/Scenes/Dog.gd
@@ -0,0 +1,19 @@
+extends KinematicBody2D
+
+var speed = Vector2()
+
+func _process(delta):
+	fall()
+	jump()
+	move_and_slide(speed, Vector2(0,-1))
+	print(is_on_floor())
+
+func fall():
+	if is_on_floor():
+		speed.y = 1
+	else:
+		speed.y += 10
+
+func jump():
+	if Input.is_action_pressed("jump") and is_on_floor():
+		speed.y = -250
